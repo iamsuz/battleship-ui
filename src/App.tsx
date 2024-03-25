11f66c10ebc17d452722c019036af81376c7b8eb
Sujit M <s.mahavarkar@gmail.com>
2024-03-26 03:17:02 +0530

    Update the player board

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import { useState } from "react";
+import React from "react";
 import "./App.css";
+import AttackBoard from "./components/ui/AttackBoard";
 import Board from "./components/ui/Board";
 
 const App: React.FC = () => {
 	return (
-		<div className="App">
-			<h1>Battleship Game</h1>
-			<Board boardSize={10} />
+		<div className="App container">
+			<h1 className="game-title">Battleship Game</h1>
+			<div className="game-layout">
+				<div className="board-wrapper offset-1">
+					<Board boardSize={10} isAttackBoard={false} />
+				</div>
+				<div className="board-wrapper offset-1">
+					<AttackBoard boardSize={10} />
+				</div>
+			</div>
 		</div>
 	);
 };

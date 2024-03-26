fbf930c702b949dfd1d6bef93d10b0f578624b26
Sujit M <s.mahavarkar@gmail.com>
2024-03-27 03:17:02 +0530

    Seprates the types

---
+++ src/components/types/Ship.ts
@@ -0,0 +1,8 @@
+type ShipType = {
+	name: string;
+	size: number;
+	color: string; // Store the ship color
+	direction: "horizontal" | "vertical"; // Track ship direction
+};
+
+export default ShipType;
